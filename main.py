6cb90b6e6db91431e5913d2364ea21027f9e5961
Michał Kruk <michalkruk199@gmail.com>
2020-04-28 20:22:17 +0200

    response type changed

--- main.py
+++ main.py
@@ -17,6 +17,11 @@ def dict_factory (cursor, row):
         dct [col [0]] = row [idx]
     return dct;
 
+def dict_factory_list (cursor, row):
+    my_string = row [0]
+    return my_string
+    
+
 @app.get ('/tracks')
 async def get_tracks (page:int = 0, per_page:int = 10):
     app.db_connection.row_factory = dict_factory
@@ -25,19 +30,16 @@ async def get_tracks (page:int = 0, per_page:int = 10):
     SELECT * FROM tracks LIMIT ? OFFSET ?''', [per_page, page]).fetchall ()
     return data
 
-@app.get ('tracks/album_id')
-async def get_names_of_album (album_id:int = 0):
+@app.get ('/tracks/album')
+async def get_names_of_album (album_id:int):
+    app.db_connection.row_factory = dict_factory
     cursor = app.db_connection.cursor ()
-    data = cursor.execute ('''
-        SELECT Name FROM tracks WHERE Album_id = ?           
-            ''', (album_id)).fetchall ()
+    data = cursor.execute ('''SELECT Name FROM tracks WHERE AlbumId = ?''', [album_id]).fetchall ()
     return data
 
 @app.get ('/tracks/composers')
 async def get_titles_of_composer (composer_name):
-   cursor = app.db_connection.cursor ()
-   data = cursor.execute ('''
-        SELECT tracks.Name FROM tracks WHERE Name = 
-   ''', [composer_name]).fetchall ()
-
-   return data
+    app.db_connection.row_factory = dict_factory_list
+    cursor = app.db_connection.cursor ()
+    data = cursor.execute ('''SELECT Name FROM tracks WHERE Composer = ? ''', [composer_name]).fetchall ()
+    return data
